884532c4b84dc9f26959b46c3c0554e234321dd5
David Kubeš <kubesdavid@email.cz>
2022-10-15 12:20:28 +0200

    chore: Refactor getRandomColor()

--- app/javascript/dashboard/helper/labelColor.js
+++ app/javascript/dashboard/helper/labelColor.js
@@ -1,8 +1,5 @@
 export const getRandomColor = () => {
-  const letters = '0123456789ABCDEF';
-  let color = '#';
-  for (let i = 0; i < 6; i += 1) {
-    color += letters[Math.floor(Math.random() * 16)];
-  }
-  return color;
+  return `#${Math.floor(Math.random() * 0x1000000)
+    .toString(16)
+    .padStart(6, '0')}`;
 };
